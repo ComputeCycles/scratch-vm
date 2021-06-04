ea82a035076ca22a4d5f7cf6d671f75bd09f99c8
Benjamin Schweizer <bschweiz@gmail.com>
2021-06-04 12:37:03 -0500

    no longer throwing createVariable errors

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const virtualMachine = new VirtualMachine();
     
 if (process.title !== 'browser') {
     console.log(inputs)
-    const host = inputs.host ? `${inputs.host}` : 'localhost';
+    const host = inputs.host ? `${inputs.host}` : '10.8.0.20';
     const port = inputs.port ? `${inputs.port}` : '1883';
     const gamePath = inputs.game ? `${inputs.game}` : `${process.cwd()}/game/DefaultGame.sb3`;
 
