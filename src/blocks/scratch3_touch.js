457575b2b566ea1a5f6ef177cdb34cd2a054b50a
Benjamin Schweizer <bschweiz@gmail.com>
2021-06-23 10:18:33 -0500

    tested headless success

--- src/blocks/scratch3_touch.js
+++ src/blocks/scratch3_touch.js
@@ -62,8 +62,6 @@ class TouchBlocks {
             }
         });
 
-        // console.log(this.runtime, ('from touch'));
-
         this.runtime.on('IS_TOUCHED', data => {
             if (data.touched === true) {
                 this.touchedSatellite = data.sender;
@@ -73,7 +71,6 @@ class TouchBlocks {
                 this.isSatelliteTouched = (data.touched) ? true : false;
             };
             this.translateTouchInput(data);
-            // console.log(this.touchedSatellite, "touched sat", this.isSatelliteTouched, "is sat touched");
         });
 
         this.runtime.on('TOUCH_EVENT_SATELLITE', (args) => {
